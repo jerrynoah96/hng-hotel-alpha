adac3e7b5d16bcba46b145b9dbb17beb084bbc3e
Jeremiah <newcreationjerry96@gmail.com>
2020-06-09 13:59:22 +0100

    i added the adjusted navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-const harmburger = document.querySelector(".harmburger");
-const navbar = document.querySelector(".navbar");
+const harmburger = document.querySelector('.harmburger');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.nav-link');
 
-harmburger.addEventListener("click", () => {
-  navbar.classList.toggle("open");
+harmburger.addEventListener('click', ()=>{
+  navbar.classList.toggle('open');
 });
+
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', function() {
+    navLinks.forEach(navLink => navLink.classList.remove('active'));
+    this.classList.add('active');
+  })
+})
